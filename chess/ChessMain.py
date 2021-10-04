372c275fd2d4cb46d2c98c8cc9530ab8002c8db3
Vipin Kumar <70451642+demonsorca@users.noreply.github.com>
2021-10-04 13:13:20 +0530

    Update ChessMain.py

--- chess/ChessMain.py
+++ chess/ChessMain.py
@@ -3,7 +3,7 @@ This is main driver file. It will be responsible for handling user input and dis
 """
 
 import pygame as p
-from Chess import ChessEngine
+from chess import ChessEngine
 
 WIDTH = HEIGHT = 720
 DIMENSION = 8
